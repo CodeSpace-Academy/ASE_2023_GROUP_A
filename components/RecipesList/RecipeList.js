49fbe033010bff94c99577b627d352a034ca2c63
Mpho Vincent Makgwe <mphomakgwe$@gmail.com>
2023-10-09 20:50:49 +0200

    Done With TimeConversions

--- components/RecipesList/RecipeList.js
+++ components/RecipesList/RecipeList.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import Link from "next/link";
-import { responsive } from "../../helpers/settings/settings";
-import { formatTime } from "../../helpers/TimeConvertor";
+import RecipeCard from "../Cards/RecipeCard";
+
 const RecipeList = () => {
   const [recipes, setRecipes] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
@@ -35,64 +31,17 @@ const RecipeList = () => {
 return(
   <div>
      <h1 className="text-3xl font-bold mb-4">Recipes</h1>
-    <div className="container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-    
-      {/* Display loading message while data is being fetched */}
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          {recipes.map((recipe) => (
-            <div
-              key={recipe._id}
-              className="bg-purple-300 p-4 rounded shadow mb-4"
-            >
-              <Link href={`/${encodeURIComponent(recipe.title)}`} key={recipe._id}>
-                <h2 className="text-2xl font-semibold">{recipe.title}</h2>
-                <p className="text-gray-600">{recipe.description}</p>
-                <p className="text-gray-600">
-                  Prep Time: {formatTime(recipe.prep)} minutes
-                </p>
-                <p className="text-gray-600">
-                  Cook Time: {formatTime(recipe.cook)} minutes
-                </p>
-                <p className="text-gray-600">Category: {recipe.category}</p>
-                <p className="text-gray-600">Servings: {recipe.servings}</p>
-                <p className="text-gray-600">
-                  Published: {new Date(recipe.published).toLocaleDateString()}
-                </p>
-                <h3 className="mt-2 text-lg font-semibold">Tags:</h3>
-                <ul className="list-disc list-inside">
-                  {recipe.tags.map((tag, index) => (
-                    <li key={index} className="text-gray-600">
-                      {tag}
-                    </li>
-                  ))}
-                </ul>
-              </Link>
-              <h3 className="mt-2 text-lg font-semibold">Images</h3>
-              <ul className="list-disc list-inside">
-                <Carousel responsive={responsive}>
-                  {recipe.images.map((image, index) => (
-                    <div key={index} className="text-gray-600">
-                      <div>
-                        <Image
-                          src={image}
-                          alt={recipe.title}
-                          width={300}
-                          height={300}
-                          className="w-full h-30"
-                        />
-                      </div>
-                    </div>
-                  ))}
-                </Carousel>
-              </ul>
-            </div>
-          ))}
-        </>
-      )}
-    </div>
+     <div className="container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            {recipes.map((recipe) => (
+              <RecipeCard key={recipe._id} recipe={recipe} />
+            ))}
+          </>
+        )}
+      </div>
     <div className="flex justify-between mt-4">
   <button
     onClick={() => handlePageChange(currentPage - 1)}
