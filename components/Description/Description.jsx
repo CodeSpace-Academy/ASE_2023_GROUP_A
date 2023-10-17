de12d8c239ee1b445c34cebe86b64df71ea189f4
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-10-17 10:17:53 +0200

    docs: add description load error handling

--- components/Description/Description.jsx
+++ components/Description/Description.jsx
@@ -1,7 +1,16 @@
-const Description =()=>{
-    return(
-        <p>Description</p>
-    )
+import DescriptionError from "../error-messages/DescriptionError";
+
+const Description = ({ recipe }) => {
+  return (
+    <div>
+      <h1 className="font-bold">Description</h1>
+      {!recipe ? (
+        <DescriptionError recipe={recipe} />
+      ) : (
+        <p className="text-slate-900">{recipe.description}</p>
+      )}
+    </div>
+  );
 };
 
 export default Description;
