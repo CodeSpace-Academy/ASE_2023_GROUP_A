caef5d1aa564bd209d633b1c98ee2854bd12b626
OratileMokgoatlheng <mokgoatlhengo@gmail.com>
2023-10-18 09:53:39 +0200

    removed the state
    the state was not being used anywhere in the code

--- components/RecipesList/RecipeList.js
+++ components/RecipesList/RecipeList.js
@@ -7,10 +7,8 @@ import LoadMoreButton from "../Buttons/LoadMore";
 const RecipeList = () => {
   const [recipes, setRecipes] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
   const [loading, setLoading] = useState(true); // Initialize loading state
   const [totalRecipes, setTotalRecipes] = useState(0); // Add totalRecipes state
-
 
   useEffect(() => {
     const fetchRecipes = async (page) => {
@@ -19,10 +17,12 @@ const RecipeList = () => {
         if (response.ok) {
           const fetchedRecipes = await response.json();
 
-          setRecipes((prevRecipes)=>[...prevRecipes, ...fetchedRecipes.recipes]);
+          setRecipes((prevRecipes) => [
+            ...prevRecipes,
+            ...fetchedRecipes.recipes,
+          ]);
           setTotalRecipes(fetchedRecipes.totalRecipes);
           setLoading(false); // Set loading to false when data is fetched
-
         } else {
           console.error("Failed to fetch recipes");
         }
@@ -42,14 +42,11 @@ const RecipeList = () => {
       <h1 className="text-3xl font-bold mb-4">Recipes</h1>
       <div className="container mx-auto p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {loading ? (
-          <Loading/>
+          <Loading />
         ) : (
           <>
             {recipes.map((recipe, index) => (
-              <Link
-                href={`/${encodeURIComponent(recipe.title)}`}
-                key={index}
-              >
+              <Link href={`/${encodeURIComponent(recipe.title)}`} key={index}>
                 <RecipeCard key={recipe._id} recipe={recipe} />
               </Link>
             ))}
@@ -57,8 +54,11 @@ const RecipeList = () => {
         )}
       </div>
 
-<LoadMoreButton handlePageChange={handlePageChange} currentPage={currentPage} totalRecipes={totalRecipes}/>
-
+      <LoadMoreButton
+        handlePageChange={handlePageChange}
+        currentPage={currentPage}
+        totalRecipes={totalRecipes}
+      />
     </div>
   );
 };
