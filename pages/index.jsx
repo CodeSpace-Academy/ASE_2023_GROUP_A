a4f93c52b41d89c23825e3b9ed64dd4a3fe57375
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-10-18 10:24:36 +0200

    docs: add env error handling fix: remove hardcoded error functionality check

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,11 @@
 import RecipeList from "../components/RecipesList/RecipeList";
+import EnvError from "./error";
 
 const Home = () => {
+  if (process.env.NEXT_PUBLIC_ENV_VAR) {
+    return <EnvError />;
+  }
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 `}
