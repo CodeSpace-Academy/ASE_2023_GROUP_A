a45d11f880846f6ef61829778b540e5f9c28fce6
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-10-18 10:16:11 +0200

    docs: create EnvError component

---
+++ pages/error/index.js
@@ -0,0 +1,9 @@
+export default function EnvError(){
+  return (
+      <span className="bg-fuchsia-200 justify-items-center">
+        <h1 className="text-red-800 p-10 font-bold text-3xl align-middle justify-items-center bg-inherit">
+          Something went wrong! Please try again
+        </h1>
+      </span>
+    );
+}
