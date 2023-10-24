5e55f9bd1013377e79e46d8f20eac67f43f74040
MOSOBELULU <mosobelulu@gmail.com>
2023-10-24 20:08:57 +0200

    added home page

--- components/LayOuts/Header/Navigation.jsx
+++ components/LayOuts/Header/Navigation.jsx
@@ -53,7 +53,7 @@ const Navigation = () => {
           </button>
         </div>
         <div className="flex flex-shrink-0 items-center">
-          <Link href={`/`}>
+          <Link href={`/home`}>
             <Image  src="/Images/logo1.png" alt="Your Company" height={80} width={60}/>
             </Link>
           </div>
