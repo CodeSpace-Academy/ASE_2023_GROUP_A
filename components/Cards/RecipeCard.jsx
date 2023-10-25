8a50d4a2dc290cdf8045a696b3d23616c0e849a1
refilwe204 <nqatyelwaangelin@gmail.com>
2023-10-25 15:23:59 +0200

    refactored code

--- components/Cards/RecipeCard.jsx
+++ components/Cards/RecipeCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import CookTime from "../TimeAndDate/TimeConvertor";
-import Link from "next/link"; // Import Link from Next.js
+import Link from "next/link"; 
 
 const RecipeCard = ({ recipe }) => {
   if (!recipe) {
--- components/LayOuts/Header/Navigation.jsx
+++ components/LayOuts/Header/Navigation.jsx
@@ -21,51 +21,62 @@ const Navigation = () => {
 
   return (
     <nav className="bg-yellow-700">
-    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <div className="relative flex h-16 items-center justify-between">
-        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          {/* <!-- Mobile menu button--> */}
-          <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-            <span className="absolute -inset-0.5"></span>
-            <span className="sr-only">Open main menu</span>
-            {/* <!--
-              Icon when menu is closed.
-  
-              Menu open: "hidden", Menu closed: "block"
-            --> */}
-            <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
-            {/* <!--
-              Icon when menu is open.
-  
-              Menu open: "block", Menu closed: "hidden"
-            --> */}
-            <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
-        <div className="flex flex-shrink-0 items-center">
-          <Link href={`/home`}>
-            <Image  src="/Images/logo1.png" alt="Your Company" height={80} width={60}/>
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/* Mobile menu button */}
+            <button
+              type="button"
+              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="absolute -inset-0.5"></span>
+              <span className="sr-only">Open main menu</span>
+              {/* Icon when menu is closed */}
+              <svg
+                className="block h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+              {/* Icon when menu is open */}
+              <svg
+                className="hidden h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+
+          <div className="flex flex-shrink-0 items-center">
+            <Link href="/home"> {/* Fix href attribute */}
+              <Image src="/Images/logo1.png" alt="Your Company" height={80} width={60} />
             </Link>
           </div>
 
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                <Link href="/dashboard"> {/* Fix href attribute */}
-                  <a className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
-                </Link>
-                <Link href="/team"> {/* Fix href attribute */}
-                  <a className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
-                </Link>
-                <Link href="/projects"> {/* Fix href attribute */}
-                  <a className="text-gray-300 hover-bg-gray-700 hover-text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
-                </Link>
-                <Link href="/calendar"> {/* Fix href attribute */}
-                  <a className="text-gray-300 hover-bg-gray-700 hover-text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
+                <Link href="/"> {/* Fix href attribute */}
+                  <span className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Recipes</span>
                 </Link>
               </div>
             </div>
