975490877febcb9b90dc9c0a93c9d7dfb0e18432
OlgaMaseng <masengolga@gmail.com>
2023-10-26 08:48:18 +0200

    fixing the hamburger menu

--- components/LayOuts/Header/Navigation.jsx
+++ components/LayOuts/Header/Navigation.jsx
@@ -20,7 +20,7 @@ const Navigation = () => {
   };
 
   return (
-    <nav className="w-full bg-red-500 fixed top-0 left-0 right-0 z-10 h-16">
+    <nav className="w-full bg-red-500 fixed top-0 left-0 right-0 z-10 h-18">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
         <div className="flex items-center justify-between">
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
@@ -155,7 +155,7 @@ const Navigation = () => {
               </button>
             </div>
             <div
-              className={`absolute top-12 left-0 ${mobileMenuOpen ? "block" : "hidden"}`}
+              className={`absolute top-12 left-0 ${mobileMenuOpen ? "bg-red-500 w-full mt-5 text-center mr-6  " : "hidden"}`}
               id="mobile-menu"
             >
               <Link
@@ -179,3 +179,68 @@ const Navigation = () => {
 };
 
 export default Navigation;
+
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import { useState } from 'react';
+
+// function NavBar() {
+//   const [navbar, setNavbar] = useState(false);
+//   return (
+//     <div>
+//       <nav className="w-full bg-red-500 fixed top-0 left-0 right-0 z-10">
+//         <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+//           <div>
+//             <div className="flex items-center justify-between py-3 md:py-5 md:block">
+//               {/* LOGO */}
+//               <Link href="/home">
+//               <Image className="text-2xl text-cyan-600 font-bold"  src="/Images/logo3.png" width={30} height={30} alt="logo" />
+//               </Link>
+//               {/* HAMBURGER BUTTON FOR MOBILE */}
+//               <div className="md:hidden">
+//                 <button
+//                   className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+//                   onClick={() => setNavbar(!navbar)}
+//                 >
+//                   {navbar ? (
+//                     <Image src="/Images/close.svg" width={30} height={30} alt="logo" />
+//                   ) : (
+//                     <Image
+//                       src="/Images/hamburger-menu.svg"
+//                       width={30}
+//                       height={30}
+//                       alt="logo"
+//                       className="focus:border-none active:border-none"
+//                     />
+//                   )}
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+//           <div>
+//             <div
+//               className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+//                 navbar ? 'p-12 md:p-0 block' : 'hidden'
+//               }`}
+//             >
+//               <ul className="h-screen md:h-auto items-center justify-center md:flex ">
+//                 <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+//                   <Link href="/recipeList" onClick={() => setNavbar(!navbar)}>
+//                     Recipe
+//                   </Link>
+//                 </li>
+//                 <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+//                   <Link href="/favourite" onClick={() => setNavbar(!navbar)}>
+//                     Favourites
+//                   </Link>
+//                 </li>
+//               </ul>
+//             </div>
+//           </div>
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// }
+
+// export default NavBar;
