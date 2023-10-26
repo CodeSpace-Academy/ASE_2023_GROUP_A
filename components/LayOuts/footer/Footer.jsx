6123dc41279d02c143f73626c63dda32dd747183
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-10-26 09:00:14 +0200

    fix: positioning and padding

--- components/LayOuts/footer/Footer.jsx
+++ components/LayOuts/footer/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer() {
   const pageName = "< Cooking Devs />";
 
   return (
-    <>
-      <div className="bg-red-700 p-10 text-white text-lg  flex space-x-10 items-center justify-between">
+    <div className="bg-red-700 p-10 text-white h-30px mb-0">
+      <div className="text-lg  flex space-x-10 items-center justify-between mb-auto">
         <Link target="_blank" href="https://www.privacypolicies.com/live/f3493839-9aed-4582-8b43-74340eb8295f">
           Privacy Policy
         </Link>
@@ -24,12 +24,10 @@ export default function Footer() {
 
         <Link href="#cookie-settings">Cookie Settings</Link>
       </div>
-      <div className="bg-red-700 p-7 text-white text-lg flex space-x-10 justify-center">
-        <p className="text-sm justify-center pl-10 pt-1 pr-10">
-          Copyright © {new Date().getFullYear()} {pageName} Pty. Ltd. | All
+        <p className="text-sm justify-center mb-0 flex pt-7">
+          Copyright © {new Date().getFullYear()} {pageName} Pty Ltd | All
           rights reserved
         </p>
-      </div>
-    </>
+    </div>
   );
 }
