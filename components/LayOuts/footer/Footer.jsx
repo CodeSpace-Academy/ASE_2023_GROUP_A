08dddfc04983533acec1d6f85833e59cabcf5596
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-10-26 08:07:44 +0200

    add footer layout

--- components/LayOuts/footer/Footer.jsx
+++ components/LayOuts/footer/Footer.jsx
@@ -1,14 +1,35 @@
+import Link from "next/link";
+
 export default function Footer() {
   const pageName = "< Cooking Devs />";
+
   return (
-    <div className="bg-red-700 p-7 text-white text-lg flex space-x-12 justify-center">
-      <h2>Contact Us</h2>
-      
-      <p className="text-sm justify-center pl-12 pt-1 pr-12">
-        Copyright © 2023 {pageName} | All rights reserved
-      </p>
-      
-      <h2>Subscribe to our newsletter</h2>
-    </div>
+    <>
+      <div className="bg-red-700 p-10 text-white text-lg  flex space-x-10 items-center justify-between">
+        <Link target="_blank" href="https://www.privacypolicies.com/live/f3493839-9aed-4582-8b43-74340eb8295f">
+          Privacy Policy
+        </Link>
+
+        <Link target="_blank" href="https://www.mars.com/cookies-english">
+          Cookie Policy
+        </Link>
+
+        <Link target="_blank" href="https://www.mars.com/legal">
+          Terms of Use
+        </Link>
+
+        <Link target="_blank" href="https://www.mars.com/accessibility">
+          Accessibility
+        </Link>
+
+        <Link href="#cookie-settings">Cookie Settings</Link>
+      </div>
+      <div className="bg-red-700 p-7 text-white text-lg flex space-x-10 justify-center">
+        <p className="text-sm justify-center pl-10 pt-1 pr-10">
+          Copyright © {new Date().getFullYear()} {pageName} Pty. Ltd. | All
+          rights reserved
+        </p>
+      </div>
+    </>
   );
 }
