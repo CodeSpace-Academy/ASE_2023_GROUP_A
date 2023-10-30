c4d6ee750d68288e56c287a9d8b37a7b31293a3e
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-10-30 09:24:10 +0200

    fix: remove sticky footer

--- components/LayOuts/footer/Footer.jsx
+++ components/LayOuts/footer/Footer.jsx
@@ -2,10 +2,11 @@ import Link from "next/link";
 
 export default function Footer() {
   const pageName = "< Cooking Devs />";
+  const currentYear = new Date().getFullYear()
 
   return (
-    <div className="bg-red-700 p-10 text-white h-screen mb-0 ">
-      <div className="text-lg  flex space-x-10 items-center justify-between mb-auto">
+    <footer className=" bg-[#ff0000] py-4 px-9 text-white w-full mb-auto bottom-0">
+      <div className="text-lg flex space-x-10 items-center justify-center mb-auto">
         <Link target="_blank" href="https://www.privacypolicies.com/live/f3493839-9aed-4582-8b43-74340eb8295f">
           Privacy Policy
         </Link>
@@ -24,10 +25,50 @@ export default function Footer() {
 
         <Link href="#cookie-settings">Cookie Settings</Link>
       </div>
-        <p className="text-sm justify-center mb-0 flex pt-7">
-          Copyright © {new Date().getFullYear()} {pageName} Pty Ltd | All
+        <p className="text-sm text-center mb-0 pt-2">
+          Copyright © {currentYear} {pageName} Pty Ltd | All
           rights reserved
         </p>
-    </div>
+    </footer>
   );
 }
+
+
+// import React from 'react'
+
+// function Footer() {
+//   return (
+    
+// <footer class="bg-red-200 rounded-lg shadow mt-auto -mb-10">
+//   <p>;lkjhbv</p>
+//     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+//         <div class="sm:flex sm:items-center sm:justify-between">
+//             <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+//                 <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+//                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+//             </a>
+//             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+//                 <li>
+//                     <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+//                 </li>
+//                 <li>
+//                     <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+//                 </li>
+//                 <li>
+//                     <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+//                 </li>
+//                 <li>
+//                     <a href="#" class="hover:underline">Contact</a>
+//                 </li>
+//             </ul>
+//         </div>
+//         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+//         <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+//     </div>
+// </footer>
+
+
+//   )
+// }
+
+// export default Footer
