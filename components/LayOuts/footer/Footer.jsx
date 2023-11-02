7cc474807ce67bb85a4f0abdb71ec7492f95d92b
refilwe204 <nqatyelwaangelin@gmail.com>
2023-11-02 14:09:18 +0200

    added footer

--- components/LayOuts/footer/Footer.jsx
+++ components/LayOuts/footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className=" bg-[#ff0000] py-4 px-9 text-white w-full mb-auto bottom-0">
+    <footer className="bg-blue-500 bottom-0 left-0 right-0 z-10 h-18">
       <div className="text-lg flex space-x-10 items-center justify-center mb-auto">
         <Link target="_blank" href="https://www.privacypolicies.com/live/f3493839-9aed-4582-8b43-74340eb8295f">
           Privacy Policy
