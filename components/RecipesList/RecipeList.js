959870035867e070fa8454e0b2c741d59f6fc5a6
Mpho Vincent Makgwe <mphomakgwe$@gmail.com>
2023-11-03 14:23:41 +0200

    still dealing with handlers

--- components/RecipesList/RecipeList.js
+++ components/RecipesList/RecipeList.js
@@ -9,173 +9,134 @@ import FloatingButton from "../Buttons/floatingButton/FloatingButton";
 // const ITEMS_PER_PAGE = 100;
 
 function RecipeList() {
+ const [recipes, setRecipes] = useState([]);
+ const [originalRecipes, setOriginalRecipes] = useState([]);
+ const [currentPage, setCurrentPage] = useState(1);
+ const [totalRecipes, setTotalRecipes] = useState(0);
+ const [autocompleteSuggestions, setAutocompleteSuggestions] = useState([]);
+ const [searchResults, setSearchResults] = useState([]);
+ const [filterResults, setFilterCategoryResults] = useState([]);
+ const [filterTagsResults, setFilterTagsResults] = useState([]);
+ const [searchQuery, setSearchQuery] = useState("");
 
-  const [recipes, setRecipes] = useState([]);
-  const [originalRecipes, setOriginalRecipes] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalRecipes, setTotalRecipes] = useState(0);
-  const [autocompleteSuggestions, setAutocompleteSuggestions] = useState([])
-  const [searchResults, setSearchResults] = useState([]);
-  const [filterResults, setFilterResults] = useState([]);
-  const [searchHistory, setSearchHistory] = useState([]);
+
   const [loading, setLoading] = useState(true);
+
+
   const favoritesContext = useContext(FavoritesContext);
   const favorites = favoritesContext.favorites || [];
 
+const loadRecipes = async (page) => {
+  const response = await fetchRecipes(page);
 
-  const loadRecipes = async (page) => {
+  if (response.ok) {
+    const data = await response.json();
+    // setRecipes([...recipes, ...data.recipes]);
+    setOriginalRecipes(data.recipes);
+    setTotalRecipes(data.totalRecipes);
+  } else {
+    console.error("Failed to fetch recipes");
+  }
+};
+ const handleLoadLess = () => {
+   setCurrentPage(currentPage - 1);
 
-    const response = await fetchRecipes(page);
-
-    if (response.ok) {
-
-      const data = await response.json();
-      // setRecipes([...recipes, ...data.recipes]);
-      setOriginalRecipes(data.recipes);
-      setTotalRecipes(data.totalRecipes);
-
-    } else {
-
-      console.error("Failed to fetch recipes");
-
-    }
-
-  };
-
-
+   loadRecipes(currentPage - 1);
+ };
   const handleLoadMore = () => {
-    
     setCurrentPage(currentPage + 1);
-   
+
     loadRecipes(currentPage + 1);
-
   };
-
   useEffect(() => {
-
     loadRecipes(currentPage);
-  setLoading(false);
+    setLoading(false);
   }, [currentPage]);
 
   if (loading) {
     return <Loading />;
   }
   const handleSearch = async (searchQuery) => {
-
     if (!searchQuery) {
-
-      
-      handleDefaultSearch()
-
+      handleDefaultSearch();
     } else {
-
       try {
-
         const response = await fetch("/api/search", {
-
           method: "POST",
           headers: {
-
             "Content-Type": "application/json",
-
           },
           body: JSON.stringify({ searchQuery }),
-
         });
 
         if (response.ok) {
-
           const searchResult = await response.json();
-          
+
           // setCount(searchResult.recipes.length)
           setSearchResults(searchResult.recipes);
-
         } else {
-
           console.error("Search request failed");
-
         }
-
       } catch (error) {
-
         console.error("Error searching recipes:", error);
-
       }
-
     }
-
   };
-
   const fetchAutocompleteSuggestions = async (searchQuery) => {
-
     try {
-
       if (searchQuery.length === 0) {
-        
         setAutocompleteSuggestions([]);
-
       } else {
-
-        const response = await fetch(`/api/autocomplete?searchQuery=${searchQuery}`);
+        const response = await fetch(
+          `/api/autocomplete?searchQuery=${searchQuery}`
+        );
 
         if (response.ok) {
-
           const data = await response.json();
           setAutocompleteSuggestions(data.autocomplete);
-
         } else {
-
           console.error("Autocomplete request failed");
-
         }
-
       }
-
     } catch (error) {
-
       console.error("Error fetching autocomplete suggestions:", error);
-
     }
-
   };
 
-  function handleDefaultSearch(){
-
+  function handleDefaultSearch() {
     setSearchResults([]);
     setAutocompleteSuggestions([]);
-
-  };
-
-  function handleDefaultFilter(){
-   
-    setFilterResults([]);
-    
-  };
-
-  let combinedResults 
-
-  if(searchResults.length <1 && filterResults.length <1){
-
-    combinedResults = [...originalRecipes];
-
-  }else{
-
-    combinedResults = [...searchResults, ...filterResults];
-
   }
-  
+
+  function handleDefaultCategoryFilter() {
+    setFilterCategoryResults([]);
+  }
+  function handleDefaultTagsFilter() {
+    setFilterTagsResults([]);
+  }
+  let combinedResults;
+
+  if (searchResults.length < 1 && filterResults.length < 1) {
+    combinedResults = [...originalRecipes];
+  } else {
+    combinedResults = [...searchResults, ...filterResults];
+  }
 
   const remainingRecipes = totalRecipes - combinedResults.length;
 
   return (
     <div>
       <Hero
-        setFilterResults={setFilterResults}
-        handleDefaultFilter={handleDefaultFilter}
+        setFilterCategoryResults={setFilterCategoryResults}
+        setFilterTagsResults={setFilterTagsResults}
+        handleDefaultCategoryFilter={handleDefaultCategoryFilter}
+        handleDefaultTagFilter={handleDefaultTagsFilter}
         handleDefaultSearch={handleDefaultSearch}
         setRecipes={setRecipes}
         onSearch={handleSearch}
         onAutocomplete={fetchAutocompleteSuggestions}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
       />
 
       <button onClick={handleDefaultSearch}>All Recipes</button>
@@ -209,17 +170,26 @@ function RecipeList() {
       </div>
 
       {combinedResults.length < totalRecipes && (
-        <div className="flex justify-center">
-          <LoadMoreButton
-            handleLoadMore={handleLoadMore}
-            remainingRecipes={remainingRecipes}
-          />
-        </div>
+        <>
+          <div className="flex justify-center">
+            <LoadButton
+              handleLoad={handleLoadLess}
+              remainingRecipes={remainingRecipes}
+              totalRecipes={totalRecipes}
+              isLoadMore={false}
+            />
+            <LoadButton
+              handleLoad={handleLoadMore}
+              remainingRecipes={remainingRecipes}
+              totalRecipes={totalRecipes}
+              isLoadMore={true}
+            />
+          </div>
+          <FloatingButton />
+        </>
       )}
-      <FloatingButton />
     </div>
   );
-
 }
 
 export default RecipeList;
