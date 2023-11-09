9b34d364f9c042a001568062a592a9ea9762e39d
Mpho Vincent Makgwe <mphomakgwe$@gmail.com>
2023-11-09 14:18:20 +0200

    done

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-
   env: {
     mongodb_password: process.env.mongodb_password,
     mongodb_username: process.env.mongodb_username,
   },
   images: {
-    domains: ['img.sndimg.com'],
+    domains: ["img.sndimg.com"],
   },
   reactStrictMode: true,
   eslint: {
-    dirs: ["pages/FavoritesPage", "components/Context/Favorites-context"],
+    dirs: ["/pages/FavoritesPage", "/components/Context/Favorites-context"],
   },
 };
