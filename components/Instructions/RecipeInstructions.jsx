c43d37d19b14b960875491abc1ef452dcc78453d
K-Magano <124664588+K-Magano@users.noreply.github.com>
2023-11-13 15:47:59 +0200

    Updated

--- components/Instructions/RecipeInstructions.jsx
+++ components/Instructions/RecipeInstructions.jsx
@@ -69,7 +69,7 @@ function RecipeInstructions({ instruction }) {
           </ol>
           <div>
             <button
-              className='bg-red-300 px-2 mx-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+              className='bg-red-400 px-2 mx-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
               onClick={handleSave}
             >
               Save
