ac08c6f56a98f2ad32ca8a8ceb86f16d86d05d2e
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-11-16 17:06:51 +0200

    docs: rewrite mongo connection string as env value

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   env: {
-    mongodb_password: process.env.mongodb_password,
-    mongodb_username: process.env.mongodb_username,
+    mongodb_uri: process.env.mongodb_uri,
   },
   images: {
     domains: ["img.sndimg.com"],
   },
   reactStrictMode: true,
   eslint: {
-    dirs: ["/pages/FavoritesPage.jsx", "/components/Context/Favorites-context.js", "/components/Cards/RecipeCard.jsx", "/components/LayOuts/Header/Navigation.jsx"],
-
+    dirs: [
+      "/pages/FavoritesPage.jsx",
+      "/components/Context/Favorites-context.js",
+      "/components/Cards/RecipeCard.jsx",
+      "/components/LayOuts/Header/Navigation.jsx",
+    ],
   },
 };
