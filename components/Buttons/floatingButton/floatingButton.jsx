6d0cff6cdf46382f1965cec2a53f90604424b47e
MOSOBELULU <mosobelulu@gmail.com>
2023-11-17 15:19:30 +0200

    merged

--- components/Buttons/floatingButton/floatingButton.jsx
+++ components/Buttons/floatingButton/floatingButton.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import { FaArrowUp, FaArrowDown } from "react-icons/fa";
-
-import { useTheme } from "../../Context/ThemeContext";
 import classes from "./floatingButton.module.css";
 
 export default function FloatingButton() {
-  const { theme } = useTheme();
   const handleScrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
@@ -15,25 +12,13 @@ export default function FloatingButton() {
       behavior: "smooth",
     });
   };
-
-  console.log('button: ', theme);
   return (
     <>
-      <div
-        className={`${theme === "light" ? "bg-blue-500" : "bg-blue-800"} ${
-          classes.floatingButton
-        }`}
-        onClick={handleScrollToTop}
-      >
+      <div className={classes.floatingButton} onClick={handleScrollToTop}>
         <FaArrowUp />
       </div>
-      <div
-        className={`${theme === "light" ? "bg-blue-500" : "bg-blue-800"} ${
-          classes.floatingButton2
-        }`}
-        onClick={handleScrollToBottom}
-      >
-        <FaArrowDown />
+      <div className={classes.floatingButton2} onClick={handleScrollToBottom}>
+        <FaArrowDown/>
       </div>
     </>
   );
