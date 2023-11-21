c9d7e91cc902a695b1781782fb8d22ec285040e2
Otshepeng Setuke <shaniquah.setuke@gmail.com>
2023-11-21 09:25:23 +0200

    fix: removed eslint errors

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,10 @@
-/* eslint-disable import/extensions */
-/* eslint-disable import/order */
-/* eslint-disable eqeqeq */
-/* eslint-disable import/no-unresolved */
 import React, { useContext, useEffect } from "react";
-import EnvError from "./error";
-import Loading from "@/components/Loading/Loading";
-import FavoritesContext from "@/components/Context/Favorites-context";
 import useSWR, { mutate } from "swr";
-import RecipeList from "@/components/RecipesList/RecipeList";
+
+import EnvError from "./error";
+import Loading from "../components/Loading/Loading";
+import FavoritesContext from "../components/Context/Favorites-context";
+import RecipeList from "../components/RecipesList/RecipeList";
 
 function Home() {
   const favoriteContext = useContext(FavoritesContext);
@@ -31,6 +28,8 @@ function Home() {
   }, [favoriteContext]); /* Add dependency array for eslint complaint */
 
   if (
+    /* prefer to compare by value not reference */
+    // eslint-disable-next-line eqeqeq
     process.env == {}
     || !process.env.mongodb_password
     || !process.env.mongodb_username
