78ba6033b899d965ec4347415aa8c668cc946d80
refilwe204 <nqatyelwaangelin@gmail.com>
2023-11-22 09:31:25 +0200

    wip

--- components/Instructions/RecipeInstructions.jsx
+++ components/Instructions/RecipeInstructions.jsx
@@ -1,97 +1,97 @@
-import React, { Fragment, useState, useEffect } from "react";
-import Loading from "../Loading/Loading";
+// import React, { Fragment, useState, useEffect } from "react";
+// import Loading from "../Loading/Loading";
 
-const RecipeInstructions = ({ recipes, onUpdateInstructions }) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [instructions, setInstructions] = useState([]);
-  const [editedInstructions, setEditedInstructions] = useState("");
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+// const RecipeInstructions = ({ recipes, onUpdateInstructions }) => {
+//   const [isEditing, setIsEditing] = useState(false);
+//   const [instructions, setInstructions] = useState([]);
+//   const [editedInstructions, setEditedInstructions] = useState("");
+//   const [error, setError] = useState(null);
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    // Delay for simulating a loading state (e.g., 2 seconds)
-    const delay = 2000;
+//   useEffect(() => {
+//     // Delay for simulating a loading state (e.g., 2 seconds)
+//     const delay = 2000;
 
-    // Set a timeout to fetch and process instructions
-    const timeoutId = setTimeout(() => {
-      try {
-        // Sort the instructions based on their index
-        const sortedInstructions = recipes.instructions.map(
-          (instruction, index) => ({ index, instruction })
-        );
-        sortedInstructions.sort((a, b) => a.index - b.index);
+//     // Set a timeout to fetch and process instructions
+//     const timeoutId = setTimeout(() => {
+//       try {
+//         // Sort the instructions based on their index
+//         const sortedInstructions = recipes.instructions.map(
+//           (instruction, index) => ({ index, instruction })
+//         );
+//         sortedInstructions.sort((a, b) => a.index - b.index);
 
-        // Map the sorted instructions to list items
-        const reorderedInstructions = sortedInstructions.map(
-          (instruction) => instruction.instruction
-        );
+//         // Map the sorted instructions to list items
+//         const reorderedInstructions = sortedInstructions.map(
+//           (instruction) => instruction.instruction
+//         );
 
-        // Set the reordered instructions and mark loading as complete
-        setInstructions(reorderedInstructions.join("\n"));
-        setEditedInstructions(reorderedInstructions.join("\n"));
-        setLoading(false);
-      } catch (error) {
-        // Handle any errors that occur during the process
-        setError("An error occurred while fetching instructions.");
-        setLoading(false);
-      }
-    }, delay);
+//         // Set the reordered instructions and mark loading as complete
+//         setInstructions(reorderedInstructions.join("\n"));
+//         setEditedInstructions(reorderedInstructions.join("\n"));
+//         setLoading(false);
+//       } catch (error) {
+//         // Handle any errors that occur during the process
+//         setError("An error occurred while fetching instructions.");
+//         setLoading(false);
+//       }
+//     }, delay);
 
-    // Cleanup the timeout to prevent memory leaks
-    return () => clearTimeout(timeoutId);
-  }, [recipes.instructions]);
+//     // Cleanup the timeout to prevent memory leaks
+//     return () => clearTimeout(timeoutId);
+//   }, [recipes.instructions]);
 
-  const toggleEditing = () => {
-    setIsEditing(!isEditing);
-  };
+//   const toggleEditing = () => {
+//     setIsEditing(!isEditing);
+//   };
 
-  const handleSaveInstructions = () => {
-    // Call an API to save the updated instructions
-    const updatedInstructions = editedInstructions.split("\n").map((instruction) => instruction.trim());
+//   const handleSaveInstructions = () => {
+//     // Call an API to save the updated instructions
+//     const updatedInstructions = editedInstructions.split("\n").map((instruction) => instruction.trim());
     
-    // Check if onUpdateInstructions is a function before calling it
-    if (typeof onUpdateInstructions === 'function') {
-      onUpdateInstructions(updatedInstructions);
+//     // Check if onUpdateInstructions is a function before calling it
+//     if (typeof onUpdateInstructions === 'function') {
+//       onUpdateInstructions(updatedInstructions);
 
-      // Update state and exit editing mode
-      setInstructions(updatedInstructions.join("\n"));
-      setIsEditing(false);
-    } else {
-      console.error("onUpdateInstructions is not a function");
-    }
-  };
+//       // Update state and exit editing mode
+//       setInstructions(updatedInstructions.join("\n"));
+//       setIsEditing(false);
+//     } else {
+//       console.error("onUpdateInstructions is not a function");
+//     }
+//   };
 
-  return (
-    <Fragment>
-      {loading ? (
-        <p>
-          <Loading />
-        </p>
-      ) : error ? (
-        <p>{error}</p>
-      ) : (
-        <div>
-          {isEditing ? (
-            <div>
-              <textarea
-                value={editedInstructions}
-                onChange={(e) => setEditedInstructions(e.target.value)}
-                rows={10}
-                cols={70}
-              />
-              <button onClick={handleSaveInstructions}>Save</button>
-              <button onClick={toggleEditing}>Cancel</button>
-            </div>
-          ) : (
-            <div>
-              <ol className="list-decimal list-inside">{instructions}</ol>
-              <button onClick={toggleEditing}>Edit Instructions</button>
-            </div>
-          )}
-        </div>
-      )}
-    </Fragment>
-  );
-};
+//   return (
+//     <Fragment>
+//       {loading ? (
+//         <p>
+//           <Loading />
+//         </p>
+//       ) : error ? (
+//         <p>{error}</p>
+//       ) : (
+//         <div>
+//           {isEditing ? (
+//             <div>
+//               <textarea
+//                 value={editedInstructions}
+//                 onChange={(e) => setEditedInstructions(e.target.value)}
+//                 rows={10}
+//                 cols={70}
+//               />
+//               <button onClick={handleSaveInstructions}>Save</button>
+//               <button onClick={toggleEditing}>Cancel</button>
+//             </div>
+//           ) : (
+//             <div>
+//               <ol className="list-decimal list-inside">{instructions}</ol>
+//               <button onClick={toggleEditing}>Edit Instructions</button>
+//             </div>
+//           )}
+//         </div>
+//       )}
+//     </Fragment>
+//   );
+// };
 
-export default RecipeInstructions;
+// export default RecipeInstructions;
--- components/Instructions/instructions.jsx
+++ components/Instructions/instructions.jsx
@@ -1,70 +1,82 @@
-import { useEffect, useState } from "react";
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import InstructionEdit from "./InstructionsEdit";
+import DescriptionError from "../error-messages/DescriptionError";
 
+function Description({ instruction, recipeId }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedInstruction, setEditedInstruction] = useState(Instruction);
 
-export default function InstructionF(ff, setFilterInstructionsResults) {
-  const [selectedInstructions, setI] = useState("");
+  const toggleEditing = () => {
+    setIsEditing(!isEditing);
+  };
 
+  // const handleEditComplete = (editedDescription) => {
+  //   setEditedDescription(editedDescription);
+  //   toggleEditing(false);
+  // };
 
-  const fetchRecipesByInstructions = async (selectedInstructions) => {
-    if (selectedInstructions < 0 || selectedInstructions == "") {
-      setFilterCategoryResults([]);
-    } else {
-      try {
-        const response = await fetch(`/api/filterbyinstructions`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            selectedInstructions: parseInt(selectedInstructions),
-          }),
-        });
-
-
-        if (response.ok) {
-          const filterInstructionsResult = await response.json();
-          // setRecipes(filterResult.recipes);
-          setFilterInstructionsResults(filterInstructionsResult.recipes);
-          // setCount(filterResult.recipes.length);
-        } else {
-          console.error("Failed to fetch recipes by instruction");
-        }
-      } catch (error) {
-        console.error("Error fetching recipes by instruction:", error);
+  const handleInstructionSave = async (newInstruction) => {
+    try {
+      const response = await fetch(`/api/Instruction/${recipeId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ description: newInstruction }),
+      });
+      if (response) {
+        setEditedInstruction(newInstruction);
+        setIsEditing(false);
+      } else {
+        console.error("Failed to update Instruction.");
       }
+    } catch (error) {
+      console.error("Error updating instruction:", error);
     }
   };
 
-
-  function handleFilterInstructions(selectedInstructions) {
-    if (parseInt(selectedInstructions) >= 0) {
-      fetchRecipesByInstructions(selectedInstructions);
-    } else {
-      setFilterInstructionsResults([]);
-    }
-  }
-
-
-  function handleChange(e) {
-    setI(e.target.value);
-  }
-
-
   return (
-    <div className="flex">
-      <input
-        type="number"
-        placeholder="Enter number of instructions..."
-        value={selectedInstructions}
-        onChange={handleChange}
-        className="border border-gray-300 rounded-l-md px-4 py-2"
-      />
-      <button
-        onClick={handleFilterInstructions}
-        className="bg-blue-500 text-white rounded-r-md px-4 py-2"
-      >
-        Filter
-      </button>
+    <div>
+      {!Instruction ? (
+        <InstructionError />
+      ) : (
+        <div>
+          {!isEditing && <p>{editedInstruction}</p>}
+          {isEditing ? (
+            <InstructionEdit
+              initialInstuction={editedDescription}
+              onSave={handleDescriptionSave}
+              toggleEditing={toggleEditing}
+            />
+          ) : (
+            <button
+              type="button"
+              className="flex items-center"
+              onClick={toggleEditing}
+            >
+              <p className="mr-2">Edit Description</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 inline"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                />
+              </svg>
+            </button>
+          )}
+        </div>
+      )}
     </div>
   );
 }
+
+export default Description;
