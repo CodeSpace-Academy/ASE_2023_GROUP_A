686274431da3e14f85a98ffeb026a24cf5da74f1
K-Magano <124664588+K-Magano@users.noreply.github.com>
2023-11-22 09:50:14 +0200

    realigned  recipe card

--- components/Recipes/Recipe.js
+++ components/Recipes/Recipe.js
@@ -8,12 +8,11 @@ import {
 import RecipeDetailTags from "../Tags/RecipeDetailTags";
 import Description from "../Description/Description";
 import Allergens from "../Allergens/allergens";
-import DropDownSVG from "../IconsAndSvg's/DropDownSVG";
+
 import CoverImage from "../Images/CoverImage";
 import IngredientsList from "../ingredients/IngredientsList";
 import RecipeInstructions from "../Instructions/RecipeInstructions";
-import UpdateRecipeInstructions from "../Instructions/editRecipeInstructions";
-import Link from "next/link"; 
+import Link from "next/link";
 import { FaArrowLeft, FaTag, FaUsers } from "react-icons/fa";
 import { FiBook } from "react-icons/fi";
 import Loading from "../Loading/Loading";
@@ -33,67 +32,78 @@ const Recipe = ({ recipe, Allergies }) => {
   }
 
   const textClass = theme === "dark" ? "text-white" : "text-black";
-  const ingredientsList = Object.entries(recipe.ingredients).map((ingredient) => `${ingredient}`);
+  const ingredientsList = Object.entries(recipe.ingredients).map(
+    (ingredient) => `${ingredient}`
+  );
 
   const firstImage = recipe.images[0];
 
   return (
-    <div className={`container mx-auto mt-24 p-4 ${textClass}`}>
-      <Link href="/">
-        <span className={`text-black-600 text-3xl ${textClass}`}>
-          <FaArrowLeft />
+    <div className={` container mx-auto mt-24 p-4 ${textClass}`}>
+      <Link href='/'>
+        <span className={`flex text-black-600 text-xl gap-2 p-4 ${textClass}`}>
+          <button className='flex border rounded p-2'>
+            {" "}
+            <FaArrowLeft className='mr-2' />
+            Back to More Recipes{" "}
+          </button>
         </span>
       </Link>
       <div
         className={`bg-${
           theme === "dark" ? "gray-700" : "white"
-        } p-4 rounded shadow mb-4 lg:flex`}
+        } p-5 m-12 rounded shadow mb-4`}
       >
-        <div className="lg:w-1/2">
-          <h1 className={`text-2xl font-bold ${textClass}`}>
-            {recipe.title}
-          </h1>
-          <CoverImage images={recipe.images} title={recipe.title} />
-          <Description description={recipe.description} recipeId={recipe._id} />
-          <div className={`mt-4 ${textClass}`}>
-            <p>
-              <FaUsers className="ml-4 mr-2" />
+        <div className=' flex items-center justify-center'>
+          <div className='  lg:w-1/2'>
+            <h1 className={`text-2xl font-bold mb-2  ${textClass}`}>
+              {recipe.title}
+            </h1>
+            <CoverImage
+              images={recipe.images}
+              title={recipe.title}
+              className='rounded'
+            />
+            <Description
+              description={recipe.description}
+              recipeId={recipe._id}
+            />
+            <div className={`mt-4 ${textClass}`}>
+              <FaUsers className='ml-4 mr-2' />
               <b>Servings</b>: {recipe.servings}
-            </p>
-          </div>
-          <div className={`mt-4 ${textClass}`}>
-            <p>
-              <FiBook className="mr-2" />
+            </div>
+            <div className={`mt-4 ${textClass}`}>
+              <FiBook className='mr-2' />
               <b>Category</b>: {recipe.category}
-            </p>
-          </div>
-          <div className={`mt-4 ${textClass}`}>
-            <FaTag className="mr-2" />
-            <b>Tags</b>
-            <RecipeDetailTags recipe={recipe} />
-          </div>
-        </div>
-        <div className={`lg:w-1/2 p-4 ${textClass}`}>
-          
-          <PrepTime prepTime={recipe.prep} />
-          <CookTime cookTime={recipe.cook} />
-          <TotalTime totalTime={recipe} />
-          <Allergens
-            allergens={Allergies}
-            recipeIngredients={ingredientsList}
-          />
-          <h3 className="mt-2 text-lg font-semibold">Ingredients:</h3>
-          <IngredientsList ingredients={Object.entries(recipe.ingredients)} />
+            </div>
+            <div className={`mt-4 ${textClass}`}>
+              <FaTag className='mr-2' />
+              <b>Tags</b>
+              <RecipeDetailTags recipe={recipe} />
+              <PrepTime prepTime={recipe.prep} />
+              <CookTime cookTime={recipe.cook} />
+              <TotalTime totalTime={recipe} />
+              <Allergens
+                allergens={Allergies}
+                recipeIngredients={ingredientsList}
+              />
+              <h3 className='mt-2 text-lg font-semibold'>Ingredients:</h3>
+              <IngredientsList
+                ingredients={Object.entries(recipe.ingredients)}
+              />
 
-          <h3 className={`text-lg font-semibold ${textClass}`}>Instructions</h3>
-          <RecipeInstructions recipes={recipe} />
+              <h3 className={`text-lg font-semibold ${textClass}`}>
+                Instructions
+              </h3>
+              <RecipeInstructions recipes={recipe} />
 
-          <div className={`${textClass} mt-4`}>
-            <Published published={recipe.published} />
+              <div className={`${textClass} mt-4`}>
+                <Published published={recipe.published} />
+              </div>
+
+              {showInstructions && <RecipeInstructions recipes={recipe} />}
+            </div>
           </div>
-
-          {showInstructions && <RecipeInstructions recipes={recipe} />}
-          <UpdateRecipeInstructions />
         </div>
       </div>
     </div>
@@ -101,6 +111,3 @@ const Recipe = ({ recipe, Allergies }) => {
 };
 
 export default Recipe;
-
-
-
