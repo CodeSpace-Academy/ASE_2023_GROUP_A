c97312066a63f1d67268852baf5eb9f00d607af1
K-Magano <124664588+K-Magano@users.noreply.github.com>
2023-11-24 10:39:58 +0200

    updated

--- components/Buttons/ViewRecipeButton/ViewRecipe.jsx
+++ components/Buttons/ViewRecipeButton/ViewRecipe.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import Link from "next/link";
 import theme from "./ViewDetails.module.css";
 
-// eslint-disable-next-line react/prop-types
 function ViewRecipeDetails({ recipe }) {
   return (
     <div className="rounded bg-blue-500 text-white p-2 m-2 transition-transform hover:scale-90 duration-300 ease-in-out">
-      {/* eslint-disable-next-line react/prop-types */}
       <Link href={`/${encodeURIComponent(recipe.title)}`}>
         <button
           type="button"
@@ -18,5 +16,4 @@ function ViewRecipeDetails({ recipe }) {
     </div>
   );
 }
-// eslint-disable-next-line eol-last
 export default ViewRecipeDetails;
