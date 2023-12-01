7d816ca9abbccaa5719e1b74aa31c7cb815e4c25
K-Magano <124664588+K-Magano@users.noreply.github.com>
2023-12-01 14:51:17 +0200

    Changed filing names

--- components/RecipesList/RecipeList.js
+++ components/RecipesList/RecipeList.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import fetchRecipes from "../../helpers/hook"
 import RecipeCard from "../Cards/RecipeCard";
 import Hero from "../Landing/hero";
-import FloatingButton from "../Buttons/floatingButton/FloatingButton";
+import FloatingButton from "../Buttons/FloatingButton/FloatingButton";
 import "react-multi-carousel/lib/styles.css";
 import FavoritesContext from "../Context/Favorites-context";
 import { useTheme } from "../../components/Context/ThemeContext"
