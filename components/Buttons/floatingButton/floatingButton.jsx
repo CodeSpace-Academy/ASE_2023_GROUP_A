b6e67befc1337bcbbf2441e14eaa8b75140e9e38
K-Magano <124664588+K-Magano@users.noreply.github.com>
2023-12-01 15:03:46 +0200

    look

--- components/Buttons/floatingButton/floatingButton.jsx
+++ components/Buttons/floatingButton/floatingButton.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from "react";
 import { FaArrowUp, FaArrowDown } from "react-icons/fa";
-import classes from "./floatingButton.module.css";
+import classes from "./FloatingButton.module.css";
 
 export default function FloatingButton() {
   const handleScrollToTop = () => {
@@ -12,14 +13,35 @@ export default function FloatingButton() {
       behavior: "smooth",
     });
   };
+
   return (
     <>
-      { /* eslint-disable-next-line jsx-a11y/no-static-element-interactions */ }
-      <div className={classes.floatingButton} onClick={handleScrollToTop}>
+      <div
+        className={classes.floatingButton}
+        onClick={handleScrollToTop}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            handleScrollToTop();
+          }
+        }}
+        role="button"
+        tabIndex="0"
+      >
         <FaArrowUp />
       </div>
-      <div className={classes.floatingButton2} onClick={handleScrollToBottom}>
-        <FaArrowDown/>
+
+      <div
+        className={classes.floatingButton2}
+        onClick={handleScrollToBottom}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            handleScrollToBottom();
+          }
+        }}
+        role="button"
+        tabIndex="0"
+      >
+        <FaArrowDown />
       </div>
     </>
   );
