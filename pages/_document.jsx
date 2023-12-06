57df6fd3b54471f1ed80f436990bf14b6fe6a83a
K-Magano <124664588+K-Magano@users.noreply.github.com>
2023-12-06 15:17:03 +0200

    commits

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import React, {
+ Html, Head, Main, NextScript
+} from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
+      <Head />
       <body>
         <Main />
         <NextScript />
